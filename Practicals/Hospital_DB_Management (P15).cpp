91f453446565621d00d68c81217e6314a652bfe9
Salazar <70525159+thisissalazar@users.noreply.github.com>
2023-05-18 15:42:12 +0530

    Create Hospital_DB_Management (P15)

---
+++ Practicals/Hospital_DB_Management (P15).cpp
@@ -0,0 +1,24 @@
+//Hospital Database Management
+
+
+/*Requirements- BaseClass
+                (
+                    Name
+                    Date of admission (Structure DD/MM/YY)
+                    Disease
+                    Date of discharge
+                    Member Function to enter info and display from DB
+                )
+                DerivedClass public: BaseClass
+                (
+                    Age of patient
+                )
+                List info about all to stoer the age of patients
+                List info about all the paediatric patients less than 12y/o
+                Use of polymorphism to call member fn of base class
+*/
+
+
+#include <iostream>
+using namespace std;
+
