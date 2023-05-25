404914ba14bcddbd0c667ce26ff47f60fff32f42
Salazar <70525159+thisissalazar@users.noreply.github.com>
2023-05-25 15:10:04 +0530

    Added Unary Operator function.

--- src/overloading/operator overloading.cpp
+++ src/overloading/operator overloading.cpp
@@ -1,1 +1,52 @@
+    /*Synax for overloading
 
+    class className {
+    ... .. ...
+    public
+       returnType operator symbol (arguments) {
+           ... .. ...
+       } 
+    ... .. ...
+
+    Here,
+};
+    'returnType' is the return type of the function.
+    'operator' is a keyword.
+    'symbol' is the operator we want to overload. Like: +, <, -, ++, etc.
+    'arguments' is the arguments passed to the function.
+
+To overload an operator a special function is used called 'operator'    
+*/    
+// Overload ++ when used as prefix
+
+#include <iostream>
+using namespace std;
+
+class Count {
+   private:
+    int value;
+
+   public:
+
+    // Constructor to initialize count to 5
+    Count() : value(5) {}
+
+    // Overload ++ when used as prefix
+    void operator ++ () {
+        ++value;
+    }
+
+    void display() {
+        cout << "Count: " << value << endl;
+    }
+};
+
+int main() {
+    Count count1;
+
+    // Call the "void operator ++ ()" function
+    ++count1;
+
+    count1.display();
+    return 0;
+}
