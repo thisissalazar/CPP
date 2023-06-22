0b59fa43b9ac83c3db63dd66711db150d4bf0bb1
Salazar <70525159+thisissalazar@users.noreply.github.com>
2023-06-22 20:57:39 +0530

    Added types of constructors

--- src/classes n objects/constructors.cpp
+++ src/classes n objects/constructors.cpp
@@ -15,10 +15,14 @@ public:
         ID = id;
         salary = sal;
     }
-    praveen() //Declaration of a default constructor.
+    /*praveen() //Declaration of a default constructor.
     {
         ID = 1;
         salary = 50;
+    }*/
+    praveen(praveen &obj)
+    {
+        salary=obj.salary;
     }
     int display()
     {
@@ -28,8 +32,11 @@ public:
 
 int main()
 {
-    praveen a;//Initializing a constructor (not parameterized)
+    // praveen a;//Initializing a constructor (not parameterized)
     praveen a(1, 5);//Parameterized constructor
+    praveen b(a);
     a.display();
+    b.display();
+    //tho ID will throw garbage value because there's no default constructor invoked nor there's any value provided (GIGO)
     return 0;
 }
